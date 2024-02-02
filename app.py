f83c53c436ddce5a38ad56a23a171772052ff8b3
gitelras <ella.rasalo@helsinki.fi>
2024-02-02 18:21:22 +0200

    :lipstick:

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from os import getenv
 
-
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
